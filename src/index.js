4b78c326f68489d0708c32fcb7f59772d2716f9c
Jiri Brabec <brabeji@gmail.com>
2016-03-07 13:54:02 +0100

    feat: createConstant utility function.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-export createReducer from 're-app/utils/createReducer';
-export createStore from 're-app/utils/createStore';
-export BlissComponent from 're-app/components/BlissComponent';
+/**
+ * utils
+ */
+import createReducer from './utils/createReducer';
+import createStore from './utils/createStore';
+import createConstants from './utils/createConstants';
+
+/**
+ * components
+ */
+import BlissComponent from 're-app/components/BlissComponent';
+
+export {
+	createReducer,
+	createStore,
+	createConstants,
+	BlissComponent
+};
