0d192cd754f8d513e4eaaff7ac3979b365404448
Jiri Brabec <brabeji@gmail.com>
2016-03-22 13:08:08 +0100

    fix: update re-app-builder to fix building of examples.

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,37 @@
 var path = require('path');
 var nodeExternals = require('webpack-node-externals');
 
-module.exports = {
-	entry: {
-		'examples/components': ['./examples/components'],
-		'utils': ['./src/utils'],
-		'decorators': ['./src/decorators'],
-		'components': ['./src/components']
+module.exports = [
+	{
+		useDefaultEntryPoints: false,
+		entry: {
+			'examples/components': ['./examples/components']
+		},
+		resolve: {
+			alias: {
+				're-app': path.join(__dirname, 'src'),
+				're-app/examples': path.join(__dirname, 'examples')
+			}
+		}
 	},
-	output: {
-		library: 're-app',
-		libraryTarget: 'umd'
-	},
-	externals: [
-		nodeExternals()
-	],
-	resolve: {
-		alias: {
-			're-app': path.join(__dirname, 'src'),
-			're-app/examples': path.join(__dirname, 'examples')
+	{
+		entry: {
+			'utils': ['./src/utils'],
+			'decorators': ['./src/decorators'],
+			'components': ['./src/components']
+		},
+		output: {
+			library: 're-app',
+			libraryTarget: 'umd'
+		},
+		externals: [
+			nodeExternals()
+		],
+		resolve: {
+			alias: {
+				're-app': path.join(__dirname, 'src'),
+				're-app/examples': path.join(__dirname, 'examples')
+			}
 		}
 	}
-};
+];
