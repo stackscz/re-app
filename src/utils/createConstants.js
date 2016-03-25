090214ebc452033b1b4e941b5eae5f63a46b39d6
Jiri Brabec <brabeji@gmail.com>
2016-03-25 18:13:19 +0100

    chore: setup tests, improve and test createConstants

--- src/utils/createConstants.js
+++ src/utils/createConstants.js
@@ -1,9 +1,36 @@
 import _ from 'lodash';
+import invariant from 'invariant';
 
+/**
+ * Utility function to create object of constants
+ *
+ * Example:
+ *
+ * 	createConstants(['A', 'B'])
+ * 	=
+ * 	{
+ * 		A: 'A',
+ * 		B: 'B'
+ * 	}
+ *
+ *
+ * 	createConstants(['A', 'B'], 'prefix-')
+ * 	=
+ * 	{
+ * 		A: 'prefix-A',
+ * 		B: 'prefix-B'
+ * 	}
+ *
+ * @param constants
+ * @param prefix
+ * @returns {*}
+ */
 export default function createConstants(constants, prefix) {
+	invariant(_.isArray(constants), 'First argument of `createConstants` should be array of strings - names of constants.');
+	invariant(_.isUndefined(prefix) || _.isString(prefix), 'Second optional argument of `createConstants` should be a string prefix to prepend to all constant values.');
 	return constants.reduce((accumulatedConstants, constantName) => {
 		return _.assign(accumulatedConstants, {
-			[constantName]: prefix + constantName
+			[constantName]: (prefix || '') + constantName
 		});
 	}, {});
 }
