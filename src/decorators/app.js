47416592d1d1a8510da6895af45107471b6b0792
Jiri Brabec <brabeji@gmail.com>
2016-04-21 14:26:49 +0200

    feat: add support for splashscreen to app decorator.

--- src/decorators/app.js
+++ src/decorators/app.js
@@ -1,21 +1,31 @@
 import React from 'react';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
+import { container } from 're-app/decorators';
 
 /**
  * Wraps component with react-redux Provider to create app component
  *
  */
-export default function app(store) {
+export default function app(store, isStateReady, splashElement) {
 	return function wrapWithApp(WrappedComponent) {
+
+		@container((state) => ({isStateReady: !isStateReady || isStateReady(state)}))
 		class Container extends React.Component {
+			render() {
+				const { isStateReady } = this.props;
+				return isStateReady ? (<WrappedComponent />) : (splashElement || <i>Loading&hellip;</i>);
+			}
+		}
+
+		class AppProvider extends React.Component {
 			render() {
 				return (
 					<Provider store={store}>
-						<WrappedComponent />
+						<Container />
 					</Provider>
 				);
 			}
 		}
-		return Container;
+		return AppProvider;
 	};
 }
