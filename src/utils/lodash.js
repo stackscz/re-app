f45c41e7ee6b13ad72d38c6e8c8554def043391c
Jiri Brabec <brabeji@gmail.com>
2016-04-21 14:18:41 +0200

    feat: create lodash mixins file, add revealNumber lodash mixin.

---
+++ src/utils/lodash.js
@@ -0,0 +1,16 @@
+import _ from 'lodash';
+
+function isNormalInteger(str) {
+	return /^\+?(0|[1-9]\d*)$/.test(str);
+}
+
+_.mixin({
+	revealNumber: (val) => {
+		if(isNormalInteger(val)) {
+			return parseInt(val, 10);
+		}
+		return val;
+	}
+});
+
+export default _;
