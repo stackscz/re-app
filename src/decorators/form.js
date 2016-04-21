8f13f7bacd0970e5885209cbf70da359d7ac4309
Jiri Brabec <brabeji@gmail.com>
2016-04-21 14:35:56 +0200

    fix: separate form config parsing function from form decorator.

--- src/decorators/form.js
+++ src/decorators/form.js
@@ -1,50 +1,20 @@
 /* eslint-disable */
 import React from 'react';
 import {reduxForm} from 'redux-form';
-import validate from 'validate.js';
-import dot from 'dot-object';
+import parseFormConfig from 're-app/utils/parseFormConfig';
 
 /**
  * Wraps component with redux-form enhanced with validation
  *
  */
 export default function form(config, mapStateToProps, mapDispatchToProps, mergeProps, options) {
-
-	const internalConfig = config ? {...config} : {};
-	if (!internalConfig.form) {
-		internalConfig.form = Math.random().toString();
-	}
-
-	// TODO parse validation
-
-	var fieldValidationsConstraints = {};
-	internalConfig.fields = internalConfig.fields.map((field) => {
-		if (_.isObject(field)) {
-			if (field.validations) {
-				fieldValidationsConstraints[field.name] = field.validations;
-			}
-			return field.name;
-		} else {
-			return field;
-		}
-	});
-
-	const reduxFormValidate = values => {
-		var errors = validate(values, fieldValidationsConstraints);
-		if (errors) {
-			dot.object(errors);
-		}
-		return errors || {};
-	};
-	internalConfig.validate = reduxFormValidate;
-
-
+	const {fieldNames, ...reduxConfig} = parseFormConfig(config);
 	return function wrapWithForm(WrappedComponent) {
-		@reduxForm(internalConfig, mapStateToProps, mapDispatchToProps, mergeProps, options)
+		@reduxForm(reduxConfig, mapStateToProps, mapDispatchToProps, mergeProps, options)
 		class FormContainer extends React.Component {
 			render() {
 				return (
-					<WrappedComponent {...this.props} />
+					<WrappedComponent {...this.props} fieldNames={fieldNames} />
 				);
 			}
 		}
