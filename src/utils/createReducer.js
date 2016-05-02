8382d1e0e1303b83b4a7ab673dd3d671e8e36ebd
Jiri Brabec <brabeji@gmail.com>
2016-05-02 10:51:18 +0200

    fix(createReducer): merge global initial state with reducer initial state properly.

--- src/utils/createReducer.js
+++ src/utils/createReducer.js
@@ -5,6 +5,9 @@ export default function createReducer(initialState, handlers) {
 	invariant(!_.isUndefined(initialState), 'First argument of `createReducer` should be value representing initial state of reducer.');
 	invariant(_.isUndefined(handlers) || _.isObject(handlers), 'Second argument of `createReducer` should be object containing reducer functions keyed by redux action type which they handle.');
 	return function reducer(state = initialState, action) {
+		if (action.type === '@@INIT') {
+			state = {...initialState, ...state};
+		}
 		if (handlers && handlers.hasOwnProperty(action.type)) {
 			return handlers[action.type](state, action);
 		} else {
