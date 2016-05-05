ede26ed03afb227388f4c3a41cd86294061c0d0a
Jiri Brabec <brabeji@gmail.com>
2016-05-05 11:51:20 +0200

    chore: lint and comment mock ApiService

--- src/mocks/ApiService.js
+++ src/mocks/ApiService.js
@@ -1,26 +1,53 @@
-/* eslint-disable */
 import _ from 'lodash';
 import { Schema, arrayOf } from 'normalizr';
 import { denormalize } from 'denormalizr';
 import invariant from 'invariant';
 
 export default {
+	/**
+	 * Called on auth module bootstrap.
+	 * It is possible to for example read auth cookie and return new auth state with cookie data in it.
+	 *
+	 * @param externalAuthContext whole state slice of api module
+	 * @returns {*} modified auth state
+	 */
 	getInitialAuthContext: (externalAuthContext) => {
-		return externalAuthContext;
+		return {
+			...externalAuthContext,
+			someDataNeededForAuthentication: {
+				possiblyClientId: 'key'
+			}
+		};
 	},
+	/**
+	 * Called on auth module bootstrap after getInitialAuthContext.
+	 * Can check if authContext represents authenticated user and resolve with new authContext patched with that user
+	 *
+	 * @param apiContext api module state slice
+	 * @param authContext auth module state slice
+	 * @returns {Promise}
+	 */
 	initializeAuth: (apiContext, authContext) => {
 		logBoldMessage('ApiService.initializeAuth called');
 		return new Promise((resolve) => {
 			// possibly set user and possibly modify authContext
 			const updatedAuthContext = {
 				...authContext,
-				user: {username: 'johndoe'},
-				credentials: {username: 'username', password: 'password'},
-				dataNeededForAuthentication: {possiblyClientId: 'key'}
+				user: {username: 'username'}
 			};
 			resolve(updatedAuthContext);
 		});
 	},
+	/**
+	 * Called on auth module LOGIN action.
+	 * Resolves with new authContext patched with user when login successful.
+	 * Rejects with error object when login failed
+	 *
+	 * @param credentials
+	 * @param apiContext
+	 * @param authContext
+	 * @returns {Promise}
+	 */
 	login: (credentials, apiContext, authContext) => {
 		logBoldMessage('ApiService.login called');
 		return new Promise((resolve, reject) => {
@@ -28,7 +55,6 @@ export default {
 				// set user and possibly modify authContext
 				resolve({
 					...authContext,
-					credentials: {username: 'username', password: 'password'},
 					user: {
 						username: credentials.username
 					}
@@ -40,24 +66,49 @@ export default {
 						{
 							message: 'Invalid credentials'
 						}
-					]
+					],
+					originalHttpResponse: {}
 				});
 			}
 		});
 	},
+	/**
+	 * Logs out user and resolves with new authContext with user removed, clears cookies etc.
+	 *
+	 * @param apiContext
+	 * @param authContext
+	 * @returns {Promise}
+	 */
 	logout: (apiContext, authContext) => {
 		return new Promise(resolve => {
 			logBoldMessage('ApiService.logout called');
 			resolve({...authContext});
 		});
 	},
+	/**
+	 * Resolves with app entity descriptors
+	 *
+	 * @param apiContext
+	 * @param authContext
+	 * @returns {Promise}
+	 */
 	getEntityDescriptors: (apiContext, authContext) => {
 		logBoldMessage('ApiService.getEntityDescriptors called');
-		return new Promise((resolve) => {
-			const { credentials } = authContext;
+		return new Promise((resolve, reject) => {
+			//const { credentials } = authContext;
+			//return reject('fofo');
 			resolve(entityDescriptors);
 		});
 	},
+	/**
+	 * Resolves with result of api call for entity collection index possibly filtered by filter param
+	 *
+	 * @param collectionName
+	 * @param filter
+	 * @param apiContext
+	 * @param authContext
+	 * @returns {Promise}
+	 */
 	getEntityIndex: (collectionName, filter, apiContext, authContext) => {
 		logBoldMessage('ApiService.getEntityIndex called');
 		invariant(_.isUndefined(filter), 'filtering is not supported in mock implementation of ApiService');
@@ -68,6 +119,15 @@ export default {
 			resolve(result);
 		});
 	},
+	/**
+	 * Resolves with single entity from given collection and with given id
+	 *
+	 * @param collectionName
+	 * @param id
+	 * @param apiContext
+	 * @param authContext
+	 * @returns {Promise}
+	 */
 	getEntity: (collectionName, id, apiContext, authContext) => {
 		logBoldMessage('ApiService.getEntity called');
 		return new Promise((resolve) => {
@@ -75,10 +135,27 @@ export default {
 			resolve(result);
 		});
 	},
+	/**
+	 * Persists single entity. Decides if create or update entity based on presence of id param
+	 *
+	 * @param collectionName
+	 * @param id
+	 * @param entity
+	 * @param apiContext
+	 * @param authContext
+	 */
 	persistEntity: (collectionName, id, entity, apiContext, authContext) => {
 		logBoldMessage('ApiService.persistEntity called');
 		invariant(false, 'persistEntity is not supported in mock implementation of ApiService');
 	},
+	/**
+	 * Deletes single entity.
+	 *
+	 * @param collectionName
+	 * @param id
+	 * @param apiContext
+	 * @param authContext
+	 */
 	deleteEntity: (collectionName, id, apiContext, authContext) => {
 		logBoldMessage('ApiService.deleteEntity called');
 		invariant(false, 'deleteEntity is not supported in mock implementation of ApiService');
@@ -89,7 +166,9 @@ const entityDescriptors = {
 	schemas: {
 		posts: {
 			name: 'posts',
+			displayFieldName: 'title',
 			idFieldName: 'id',
+			isFilterable: false,
 			fields: {
 				title: {
 					name: 'title',
@@ -105,6 +184,8 @@ const entityDescriptors = {
 		tags: {
 			name: 'tags',
 			idFieldName: 'name',
+			displayFieldName: 'name',
+			isFilterable: false,
 			fields: {
 				name: {
 					name: 'name',
