19bbd52dd211d1e0d3fa6bac65657c8da6358d9c
Jiri Brabec <brabeji@gmail.com>
2016-05-10 20:34:42 +0200

    fix(entityDescriptors): fix selectors to return undefined properly

--- src/modules/entityDescriptors/selectors.js
+++ src/modules/entityDescriptors/selectors.js
@@ -1,4 +1,10 @@
-export const getEntitySchemas = (state) => state.entityDescriptors.schemas;
-export const getEntityFieldsets = (state) => state.entityDescriptors.fieldsets;
-export const getEntitySchemaGetter = (collectionName) => (state) => state.entityDescriptors.schemas[collectionName];
-export const getEntityMappingGetter = (collectionName) => (state) => state.entityDescriptors.mappings[collectionName];
+import _ from 'lodash';
+
+export const getEntitySchemas = (state) => state.entityDescriptors ? state.entityDescriptors.schemas : undefined;
+export const getEntityFieldsets = (state) => state.entityDescriptors ? state.entityDescriptors.fieldsets : undefined;
+export const getEntitySchemaGetter = (collectionName) => (state) => {
+	return _.get(state, ['entityDescriptors', 'schemas', collectionName]);
+};
+export const getEntityMappingGetter = (collectionName) => (state) => {
+	return _.get(state, ['entityDescriptors', 'mappings', collectionName]);
+};
