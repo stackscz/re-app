b23879d08134617abf2b6849be2a55d4c376e11f
Jiri Brabec <brabeji@gmail.com>
2016-05-27 10:52:25 +0200

    chore: create webpack aliases for src groups

--- babel-alias.js
+++ babel-alias.js
@@ -3,7 +3,14 @@ var path = require('path');
 module.exports = {
 	resolve: {
 		alias: {
-			're-app': path.join(__dirname, 'src')
+			're-app': path.join(__dirname, 'src'),
+			'components': path.join(__dirname, 'src', 'components'),
+			'containers': path.join(__dirname, 'src', 'containers'),
+			'decorators': path.join(__dirname, 'src', 'decorators'),
+			'effects': path.join(__dirname, 'src', 'effects'),
+			'mocks': path.join(__dirname, 'src', 'mocks'),
+			'modules': path.join(__dirname, 'src', 'modules'),
+			'utils': path.join(__dirname, 'src', 'utils')
 		}
 	}
 };
