324d86a8c9c1ce37f4bf1c5be647c4a9317b23df
Jiri Brabec <brabeji@gmail.com>
2016-06-29 11:27:09 +0200

    chore(modules/auth): test reducer.

---
+++ test/modules/auth/reducer.js
@@ -0,0 +1,219 @@
+/* eslint-disable */
+import expect from 'expect';
+import Immutable from 'seamless-immutable';
+import reducer from 'modules/auth/reducer';
+import {
+	INIT,
+} from 'utils/actions';
+import {
+	INITIALIZE,
+	INITIALIZE_FINISH,
+	LOGIN,
+	LOGIN_FAILURE,
+	LOGIN_SUCCESS,
+	LOGOUT,
+	LOGOUT_FAILURE,
+	LOGOUT_SUCCESS,
+} from 'modules/auth/actions';
+
+describe('modules/auth/reducer', () => {
+
+	it('should return the initial state', () => {
+		expect(
+			reducer(undefined, {})
+		).toEqual(
+			{
+				authenticating: false,
+				errors: [],
+				initialized: false,
+				initializing: false,
+				user: null,
+			}
+		)
+	});
+
+	describe(`should handle ${INITIALIZE}`, () => {
+		it('should set initializing flag', () => {
+			expect(
+				reducer(
+					undefined,
+					{
+						type: INITIALIZE,
+						payload: {
+							some: 'context',
+						},
+					}
+				)
+			).toEqual(
+				{
+					authenticating: false,
+					errors: [],
+					initialized: false,
+					initializing: true,
+					user: null,
+					some: 'context',
+				}
+			)
+		});
+	});
+
+	describe(`should handle ${INITIALIZE_FINISH}`, () => {
+		it('should set initializing and initialized flags', () => {
+			expect(
+				reducer(
+					undefined,
+					{
+						type: INITIALIZE_FINISH,
+						payload: {
+							someMore: 'context',
+						},
+					}
+				)
+			).toEqual(
+				{
+					authenticating: false,
+					errors: [],
+					initialized: true,
+					initializing: false,
+					user: null,
+					someMore: 'context',
+				}
+			)
+		});
+	});
+
+	describe(`should handle ${LOGIN}`, () => {
+		it('should set authenticating flag.', () => {
+			expect(
+				reducer(
+					undefined,
+					{
+						type: LOGIN,
+					}
+				)
+			).toEqual(
+				{
+					authenticating: true,
+					errors: [],
+					initialized: false,
+					initializing: false,
+					user: null,
+				}
+			)
+		});
+	});
+
+	describe(`should handle ${LOGIN_FAILURE}`, () => {
+		it('should throw invalid payload.', () => {
+			expect(() => {
+				reducer(
+					undefined,
+					{
+						type: LOGIN_FAILURE,
+					}
+				);
+			}).toThrow(/invalid payload/);
+		});
+		it('should set errors.', () => {
+			expect(
+				reducer(
+					undefined,
+					{
+						type: LOGIN_FAILURE,
+						payload: {
+							error: {
+								code: 100,
+							}
+						},
+					}
+				)
+			).toEqual(
+				{
+					authenticating: false,
+					errors: [
+						{
+							code: 100,
+						}
+					],
+					initialized: false,
+					initializing: false,
+					user: null,
+				}
+			)
+		});
+	});
+
+	describe(`should handle ${LOGIN_SUCCESS}`, () => {
+		it('should throw invalid payload.', () => {
+			expect(() => {
+				reducer(
+					undefined,
+					{
+						type: LOGIN_SUCCESS,
+					}
+				);
+			}).toThrow(/invalid payload/);
+		});
+		it('should set user.', () => {
+			expect(
+				reducer(
+					undefined,
+					{
+						type: LOGIN_SUCCESS,
+						payload: {
+							user: {
+								name: 'john',
+							}
+						},
+					}
+				)
+			).toEqual(
+				{
+					authenticating: false,
+					errors: [],
+					initialized: false,
+					initializing: false,
+					user: {
+						name: 'john',
+					},
+				}
+			)
+		});
+	});
+
+	describe(`should handle ${LOGOUT_SUCCESS}`, () => {
+		it('should unset user.', () => {
+			let state = reducer(
+				Immutable.from({
+					user: { name: 'john' },
+				}),
+				{
+					type: INIT,
+				}
+			);
+			state = reducer(
+				state,
+				{
+					type: LOGOUT_SUCCESS,
+				}
+			);
+			expect(
+				reducer(
+					state,
+					{
+						type: LOGOUT_SUCCESS,
+					}
+				)
+			).toEqual(
+				{
+					authenticating: false,
+					errors: [],
+					initialized: false,
+					initializing: false,
+					user: null,
+				}
+			)
+		});
+	});
+
+});
