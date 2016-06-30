7d4dbc3fe75462c65c8d92c1c42bfbd60587c52d
Jiri Brabec <brabeji@gmail.com>
2016-06-30 14:15:34 +0200

    feat(utils/typeInvariant): support message parameters

--- src/utils/typeInvariant.js
+++ src/utils/typeInvariant.js
@@ -2,15 +2,14 @@ import invariant from 'invariant';
 import t from 'tcomb';
 import { validate } from 'tcomb-validation';
 
-export default function typeInvariant(value, type, message) {
+export default function typeInvariant(value, type, message, ...messageParams) {
 	invariant(type && t.isType(type), '"type" passed into typeInvariant must be tcomb type');
 	const validationResult = validate(value, type);
 	const firstError = validationResult.firstError();
 	const tcombMessage = firstError && firstError.message;
 	invariant(
 		validationResult.isValid(),
-		'Type validation failed:\n\n%s\n\n%s',
-		tcombMessage,
-		message ? `INFO: ${message}` : ''
+		`Type validation failed:\n\n${tcombMessage}\n\n${message ? `INFO: ${message}` : ''}`,
+		...messageParams
 	);
 }
