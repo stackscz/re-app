782805375fceafbcb970c2c212a31b4eaa22721c
Jiri Brabec <brabeji@gmail.com>
2016-07-04 22:42:03 +0200

    chore(utils) remove unused immutabilityHelper.js

--- src/utils/immutabilityHelper.js
+++
@@ -1,9 +0,0 @@
-import update from 'immutability-helper';
-
-update.extend('$delete', (value, original) => {
-	const result = update(original, { [value]: { $set: undefined } });
-	delete result[value];
-	return result;
-});
-
-export default update;
